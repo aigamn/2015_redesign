507fd52c27ef5d877481920da6aee58341e963eb
Pete Klein <peteklein23@gmail.com>
2015-01-18 13:46:46 -0600

    Broke header into includes

---
+++ prototype/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AIGA Minnesota Redesign</title>
+
+	
+
+	<script src='http://code.jquery.com/jquery-2.1.3.js'></script>
+
+</head>
+<body>
+	<header class='site-header'></header>
