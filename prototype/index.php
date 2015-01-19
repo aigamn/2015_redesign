44dd14196b3ca8b653db02d3045c744e0a752855
Pete Klein <peteklein23@gmail.com>
2015-01-19 15:54:50 -0600

    Incremental checkin -- carousel is working

--- prototype/index.php
+++ prototype/index.php
@@ -3,7 +3,8 @@
 
 <div class='container' id='home'>
 	<h1 class='text-center'>
-		 AIGA Minnesota is the first place to turn for inspiration, professional development, and excellence in design in Minnesota. 
+		 AIGA Minnesota is the first place to turn for inspiration, professional development,<br class='hidden-xs'>
+		 and excellence in design in Minnesota. 
 	</h1>
 
 	<div class='social text-center'>
@@ -42,22 +43,47 @@
 			<a href='#' class='pull-right'>All Upcoming Events</a>
 			<div class='clearfix'></div>
 		</header>
-		<div class='carousel'>
-			<div class='carousel-inner'>
+
+		<!--<div class='carousel slide hidden-sm hidden-xs' data-ride='carousel'>-->
+		<div class='carousel slide hidden-sm hidden-xs'>
+
+			<!-- Wrapper for slides -->
+			<div class='carousel-inner col-md-8' role='listbox' >
 				<div class='item active'>
 					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+					<div class='carousel-caption'></div>
 				</div>
 				<div class='item'>
 					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+					<div class='carousel-caption'></div>
 				</div>
 				<div class='item'>
 					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+					<div class='carousel-caption'></div>
 				</div>
 				<div class='item'>
 					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+					<div class='carousel-caption'></div>
 				</div>
-			</div> <!-- end .carousel-inner -->
-		</div> <!-- end .carousel -->
+			</div>
+
+			<ul class='tiles col-md-4 list-unstyled carousel-indicators border-bottom'>
+				<li data-target='.carousel' data-slide-to='0' class='active'>
+					<span class='state-indicator'></span>
+				</li>
+				<li data-target='.carousel' data-slide-to='1'>
+					<span class='state-indicator'></span>
+				</li>
+				<li data-target='.carousel' data-slide-to='2'>
+					<span class='state-indicator'></span>
+				</li>
+				<li data-target='.carousel' data-slide-to='3'>
+					<span class='state-indicator'></span>
+				</li>
+			</ul>
+			
+			<div class='clearfix'></div>
+		</div>
 	</section>
 
 	<section>
@@ -68,16 +94,83 @@
 			<a href='#' class='pull-right'>All Communities</a>
 			<div class='clearfix'></div>
 		</header>
-		<ul class='tiles list-unstyled'>
-			<li class='col-sm-4'>
+		<ul class='tiles list-unstyled border-right border-bottom clearfix'>
+			<li class='col-sm-4 active'>
+				<span class='state-indicator'></span>
 				<h3>
-					
+					Design for Good: Monthly Meeting
 				</h3>
+				<small>
+					Thursday, October 16th
+				</small>
+			</li>
+			<li class='col-sm-4'>
+				<span class='state-indicator'></span>
+				<h3>
+					Career Practice: Monthly Meeting
+				</h3>
+				<small>
+					Friday, October 17th
+				</small>
+			</li>
+			<li class='col-sm-4'>
+				<span class='state-indicator'></span>
+				<h3>
+					Design for Good: Monthly Meeting
+				</h3>
+				<small>
+					Thursday, October 16th
+				</small>
 			</li>
 		</ul>
 	</section>
-				
-	<img src='http://placehold.it/380x254/94deff/84CeEf' alt=''>
+
+	<section class='col-sm-4 border-right'>
+		<img class='img-responsive margin-auto' src='http://placehold.it/380x254/94deff/84CeEf' alt=''>
+	</section>
+		
+	<section class='padded-left col-sm-8'>
+		<header>
+			<h2 class='pull-left'>
+				Blog
+			</h2>
+			<a href='#' class='pull-right'>All Blog Entries</a>
+			<div class='clearfix'></div>
+		</header>
+		<ul class='tiles list-unstyled border-bottom'>
+			<li class='active'>
+				<span class='state-indicator'></span>
+				<h3>
+					Reaching and Growing with Jeff Holmberg
+				</h3>
+				<small>Thursday, October 16th</small>
+				<p>
+					Many freelancers and designers gathered together to hear inspiring words and life stories by Jeff Holmberg of Holmberg Design Co. at our July AIGA Minnesota Luncheon event... 
+				</p>
+			</li>
+			<li>
+				<span class='state-indicator'></span>
+				<h3>
+					Reaching and Growing with Jeff Holmberg
+				</h3>
+				<small>Thursday, October 16th</small>
+				<p>
+					Many freelancers and designers gathered together to hear inspiring words and life stories by Jeff Holmberg of Holmberg Design Co. at our July AIGA Minnesota Luncheon event... 
+				</p>
+			</li>
+			<li>
+				<span class='state-indicator'></span>
+				<h3>
+					Reaching and Growing with Jeff Holmberg
+				</h3>
+				<small>Thursday, October 16th</small>
+				<p>
+					Many freelancers and designers gathered together to hear inspiring words and life stories by Jeff Holmberg of Holmberg Design Co. at our July AIGA Minnesota Luncheon event... 
+				</p>
+			</li>
+		</ul>
+	</section>
+	
 
 </div> <!-- end .container -->
 
