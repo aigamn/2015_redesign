60296e8fb609258d044481234e4f867b16a8c245
Pete Klein <peteklein23@gmail.com>
2015-01-19 11:32:35 -0600

    Included needed CSS and JS. Began adding content.

--- prototype/header.php
+++ prototype/header.php
@@ -4,10 +4,19 @@
 	<meta charset="UTF-8">
 	<title>AIGA Minnesota Redesign</title>
 
-	
+	<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700,400italic,700italic,600italic' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' href='icomoon/style.css'>
+	<link rel='stylesheet' href='style.css'>
 
 	<script src='http://code.jquery.com/jquery-2.1.3.js'></script>
+	<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.js'></script>
 
 </head>
 <body>
-	<header class='site-header'></header>
+	<header class='site-header'>
+		<img class='logo' src='http://placehold.it/60x42/94deff/94deff' alt='logo'>
+		<ul class='list-unstyled'>
+			<li></li>
+		</ul>
+	</header>
--- prototype/index.php
+++ prototype/index.php
@@ -1,6 +1,77 @@
 
 <?php include_once('header.php'); ?>
 
-homepage content
+<div class='container'>
+	<h1>
+		 AIGA Minnesota is the first place to turn for inspiration, professional development, and excellence in design in Minnesota. 
+	</h1>
+
+	<div class='social'>
+		<small class='text-uppercase'>Connect with AIGA Minnesota</small>
+		<ul class='list-inline'>
+			<li>
+				<a href='#'>
+					<span class='icon-facebook'></span>
+				</a>
+			</li>
+			<li>
+				<a href='#'>
+					<span class='icon-twitter'></span>
+					
+				</a>
+			</li>
+			<li>
+				<a href='#'>
+					<span class='icon-instagram'></span>
+				</a>
+					
+			</li>
+			<li>
+				<a href='#'>
+					<span class='icon-linkedin'></span>
+				</a>
+			</li>
+		</ul>
+	</div> <!-- end .social -->
+
+	<section>
+		<header>
+			<h2 class='pull-left'>
+				Upcoming Events
+			</h2>
+			<a href='#' class='pull-right'>All Upcoming Events</a>
+			<div class='clearfix'></div>
+		</header>
+		<div class='carousel'>
+			<div class='carousel-inner'>
+				<div class='item active'>
+					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+				</div>
+				<div class='item'>
+					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+				</div>
+				<div class='item'>
+					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+				</div>
+				<div class='item'>
+					<img src='http://placehold.it/768x432/94deff/84CeEf' alt=''>
+				</div>
+			</div> <!-- end .carousel-inner -->
+		</div> <!-- end .carousel -->
+	</section>
+
+	<section>
+		<header>
+			<h2 class='pull-left'>
+				Communities
+			</h2>
+			<a href='#' class='pull-right'>All Communities</a>
+			<div class='clearfix'></div>
+		</header>
+	</section>
+				
+	<img src='http://placehold.it/380x254/94deff/84CeEf' alt=''>
+
+</div> <!-- end .container -->
 
 <?php include_once('footer.php'); ?>
